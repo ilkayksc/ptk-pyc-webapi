f4f9424dab5cdc8210ba5ab5ed6d88aea87d0a8d
Recep <recep.yesil@payolog.com>
2022-08-18 19:56:40 +0300

    devs

--- PycWebApi/Program.cs
+++ PycWebApi/Program.cs
@@ -11,7 +11,7 @@ namespace PycWebApi
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)     // todo add logging
+            Host.CreateDefaultBuilder(args)     
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
